5cbcd6ea09326fd907c4c7fd5e774d093f139c5e
dan <popescudand@gmail.com>
2020-10-15 13:15:34 +0300

    Finish week 5 project

--- tools/demoAsm/fillRectangle.asm
+++ tools/demoAsm/fillRectangle.asm
@@ -1,39 +1,39 @@
-@SCREEN
-D=A
-@addr
-M=D
+    @SCREEN
+    D=A
+    @addr
+    M=D
 
-@R0
-D=M
-@n
-M=D
+    @R0
+    D=M
+    @n
+    M=D
 
-@i
-M=0
+    @i
+    M=0
 
 (LOOP)
-@i
-D=M
-@n
-D=D-M
-@END
-D;JEQ
+    @i
+    D=M
+    @n
+    D=D-M
+    @END
+    D;JEQ
 
-@addr
-A=M
-M=-1
+    @addr
+    A=M
+    M=-1
 
-@32
-D=A
-@addr
-M=D+M
+    @32
+    D=A
+    @addr
+    M=D+M
 
-@i
-M=M+1
+    @i
+    M=M+1
 
-@LOOP
-0;JMP
+    @LOOP
+    0;JMP
 
 (END)
-@END
-0;JMP
+    @END
+    0;JMP
